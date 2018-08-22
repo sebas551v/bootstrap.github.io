a4ef7f5935093f57cd1f04534a6c5a309ea7bff9
unknown <luisfelipegarciacsj@gmail.com>
2018-08-22 11:40:58 -0500

    First Commit Luis

--- funtions.js
+++ funtions.js
@@ -1,3 +1,18 @@
+
 function ingresar(){
     window.location.href = "labels.html";
+
+}
+
+
+function ingressar(){
+    var text = document.getElementById("inputUser").value;
+    var pass =document.getElementById('inputPassword').value;
+
+   // if (text == "admin" && pass  == "admin" ){
+    window.location.href = "labels.html";
+       // window.open ("labels.html",) ;
+   // }else {
+    //    alert("Error en usuario");
+    //}
 }
